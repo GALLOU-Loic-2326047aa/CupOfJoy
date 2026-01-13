ab0c3ceea5d9a23e5c47781ab4c470d0aa5a2615
bapti <baptiste.armani@gmail.com>
2026-01-13 11:28:56 +0100

    Fichier permettant d'aller sur la page des abonnements des clients

--- modules/ps_stripe_subscriptions/controllers/front/default.php
+++ modules/ps_stripe_subscriptions/controllers/front/default.php
@@ -1,44 +1,41 @@
 <?php
 
-class Ps_Stripe_SubscriptionsDefaultModuleFrontController extends ModuleFrontController {
-    public $auth = true;
-    public $ssl = true;
-
-    public function initContent() {
+class Ps_Stripe_SubscriptionsSubscriptionsModuleFrontController extends ModuleFrontController
+{
+    public function initContent()
+    {
         parent::initContent();
-        $customer = $this->context->customer;
 
-        $stripe_customer_id = $this->module->createOrGetStripeCustomer(
-            $customer->id,
-            $customer->email,
-            $customer->firstname,
-            $customer->lastname
-        );
-
-        if($stripe_customer_id) {
-            $this->context->smarty->assign(
-                'stripe_error',
-                $this->module->l('Impossible de lier le compte au service de paiement.'));
-        }
-        else {
-            $subscription_data = $this->getSubscriptionData($stripe_customer_id);
-            $this->context->smarty->assign([
-                'stripe_customer_id' => $stripe_customer_id,
-                'subscription_status' => $subscription_data['status'],
-                'subscription_next_bill' => $subscription_data['next_bill'],
-                'subscription_items' => $subscription_data['items'],
-                'page_title' => $this->module->l('Gestion de mes abonnements')
-            ]);
+        // 1. On vérifie si le client est connecté
+        if (!$this->context->customer->isLogged()) {
+            Tools::redirect('index.php?controller=authentication');
         }
 
-        $this->setTemplate('module:ps_stripe_subscriptions/views/templates/subscription_management.tpl');
-    }
-    protected function getSubscriptionData($stripe_customer_id) {
-        return [
-            'status' => 'active',
-            'next_bill' => date('d-m-Y', strtotime('+1 month')),
-            'items' => [['name' => 'Abonnement Pro Mensuel', 'price' => 10.00]],
-        ];
-    }
+        $this->module->loadModuleClasses();
+        $this->module->initStripeApi();
 
+        // 2. On récupère les abonnements Stripe du client
+        $subscriptions = [];
+        try {
+            // On cherche l'ID client Stripe associé à l'ID PrestaShop
+            $stripeCustomerId = Db::getInstance()->getValue('
+                SELECT stripe_customer_id FROM ' . _DB_PREFIX_ . 'baba_stripe_customer_link 
+                WHERE id_customer = ' . (int)$this->context->customer->id
+            );
+
+            if ($stripeCustomerId) {
+                $stripeSubs = \Stripe\Subscription::all(['customer' => $stripeCustomerId]);
+                $subscriptions = $stripeSubs->data;
+            }
+        } catch (Exception $e) {
+            $this->errors[] = $this->module->l('Impossible de charger vos abonnements.');
+        }
+
+        $this->context->smarty->assign([
+            'subscriptions' => $subscriptions,
+            'my_account_url' => $this->context->link->getPageLink('my-account'),
+        ]);
+
+        $this->setTemplate('module:ps_stripe_subscriptions/views/templates/front/subscriptions.tpl');
+    }
 }
